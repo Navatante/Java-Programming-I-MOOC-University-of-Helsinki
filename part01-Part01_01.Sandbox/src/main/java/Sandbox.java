7239a8f0bb4658f5e21ca1b3ae346f5d6814d638
Jon Carbonell <navatante@proton.me>
2024-02-10 15:37:26 +0100

    Add files via upload

---
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -0,0 +1,23 @@
+
+public class Sandbox {
+
+    public static void main(String[] args) {
+       int number = 0;
+   
+       while (true) {
+           number = number + 1;
+   
+           if (number >= 5) {
+               break;
+           }
+   
+           if (number < 5) {
+               continue;
+           }
+   
+           System.out.print(number + " ");
+       }
+   
+       System.out.print(number + " ");
+   }
+   }
