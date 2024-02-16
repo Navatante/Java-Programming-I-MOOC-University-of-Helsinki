4ac1a6e809d7ae8ec95385203b3e6889d9e212f7
Jon Carbonell <navatante@proton.me>
2024-02-16 17:37:10 +0100

    Add files via upload

---
+++ part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class SquareRootOfSum {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int first = Integer.valueOf(scanner.nextLine());
+        int second = Integer.valueOf(scanner.nextLine());
+        double squareRoot = (int) Math.sqrt(second + first);
+        System.out.println(squareRoot);
+
+    }
+}
