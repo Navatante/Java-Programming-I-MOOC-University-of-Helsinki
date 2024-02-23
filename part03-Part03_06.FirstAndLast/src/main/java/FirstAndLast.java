39d091601537fe761e1d15b6ce772863cdadfc37
Jon Carbonell <navatante@proton.me>
2024-02-23 12:16:18 +0100

    Add files via upload

---
+++ part03-Part03_06.FirstAndLast/src/main/java/FirstAndLast.java
@@ -0,0 +1,23 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class FirstAndLast {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<String> list = new ArrayList<>();
+        int count = -1;
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+
+            list.add(input);
+            count++;
+        }
+        System.out.println(list.get(0)+"\n"+list.get(count));
+    }
+}
