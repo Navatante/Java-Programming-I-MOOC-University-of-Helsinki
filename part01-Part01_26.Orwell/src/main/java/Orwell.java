a83df3321ec44891d3d5d655e4bfa0feed21c261
Jon Carbonell <navatante@proton.me>
2024-03-05 21:46:01 +0100

    Update Orwell.java

--- part01-Part01_26.Orwell/src/main/java/Orwell.java
+++ part01-Part01_26.Orwell/src/main/java/Orwell.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class Orwell {
 
     public static void main(String[] args) {
+        
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Give a number:");
